ca2a56c1822f127014076f3b85f9d7e2f337325c
Jaakko Hannikainen <jgke@jgke.fi>
2021-08-25 10:50:17 +0300

    Add doc+test for collection!()

--- src/collection.rs
+++ src/collection.rs
@@ -1,3 +1,21 @@
+//! vec!-like generic collection macro.
+
+/// Generate any collection.
+///
+/// ```
+/// # #[macro_use] extern crate syns;
+/// # fn main() {
+/// use std::collections::{HashMap, HashSet};
+/// let set: HashSet<i32> = [1, 2, 3].iter().copied().collect();
+/// let macro_set: HashSet<i32> = collection!(1, 2, 3);
+/// assert_eq!(macro_set, set);
+///
+/// let map: HashMap<i32, i32> = [(1, 2), (3, 3), (2, 4)].iter().copied().collect();
+/// let macro_map: HashMap<i32, i32> = collection!(1 => 2, 2 => 4, 3 => 3);
+/// assert_eq!(macro_map, map);
+/// # }
+/// ```
+#[macro_export]
 macro_rules! collection {
     // map-like
     ($($k:expr => $v:expr),* $(,)?) => {{

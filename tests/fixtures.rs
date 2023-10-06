f866342c0b9e21bf0e61e0609dbf82df9dfc0fed
Jaakko Hannikainen <jgke@jgke.fi>
2023-10-06 14:06:48 +0300

    Show how to regen fixtures

--- tests/fixtures.rs
+++ tests/fixtures.rs
@@ -4,6 +4,7 @@ use std::path::PathBuf;
 use std::process::Command;
 use test_generator::test_resources;
 
+// for f in test-files/hello/*; do syns --no-color -o '\.' $f > tests/fixtures/nocolor/$(basename $f); done
 #[test_resources("test-files/hello/*")]
 fn hello_world_langs_nocolor(file: &str) {
     let mut cmd = Command::cargo_bin("syns").unwrap();
@@ -21,7 +22,7 @@ fn hello_world_langs_nocolor(file: &str) {
         .stdout(expected_output);
 }
 
-
+// for f in test-files/hello/*; do syns --color '\.' $f > tests/fixtures/color/$(basename $f); done
 #[test_resources("test-files/hello/*")]
 fn hello_world_langs_color(file: &str) {
     let mut cmd = Command::cargo_bin("syns").unwrap();
